ba269e7f2ed9a83f39158f8c3cd2f9430c507635
Justunator <quanjustin@ymail.com>
2026-05-04 19:50:17 -0600

    Converted page to simple input feature
    ability to submit new items into a list that will automatically display stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
-import TopBar from "./components/TopBar";
-import './App.css'
+import React, { useState } from 'react'
+import "./App.css";
 
-function App() {
+function Card({ title }: { title: string }) {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
-    <div className="App">
-      <TopBar className="top-bar" />
+    <div className='card'>
+      <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "Liked!" : "Like"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+function App() {
+  const [stuff, setStuff] = useState<Set<string>>(new Set());
+  
+  return (
+    <div className='App'>
+      <h1>List of stuff</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const newStuff = formData.get("new-stuff") as string;
+        setStuff(new Set([...stuff, newStuff]));
+        e.currentTarget.reset();
+      }}>
+        <input name="new-stuff" placeholder='Add new stuff' />
+        <button type='submit'>Submit</button>
+      </form>
+      <button onClick={() => setStuff(new Set())}>Clear stuff</button>
+      {stuff.size > 0 ? Array.from(stuff).map((item) => (
+        <Card title={item} />
+      )) : <p>Nothing!</p>}
+    </div>
+  );
+}
+
+export default App;
